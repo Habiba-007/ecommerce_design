abe55eb1ce610c496e33cfc4b593c3fea3f8de98
jueab1 <hoque273@gmail.com>
2024-04-30 02:50:51 +0600

    30.4.24 something update here

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,18 @@
-function decrementCart() {
+function decrementCart(price_target) {
   event.preventDefault()
   let input = event.currentTarget.nextElementSibling;
   input.value = parseInt(input.value) - 1;
 
-  let price = document.getElementById('price_1');
+  let price = document.getElementById(price_target);
   price.innerText = parseFloat(price.dataset.price) * input.value;
 }
 
-function incrementCart() {
+function incrementCart(price_target) {
   event.preventDefault();
   let input = event.currentTarget.previousElementSibling;
   input.value = parseInt(input.value) + 1;
 
-  let price = document.getElementById('price_1')
+  let price = document.getElementById(price_target)
   price.innerText = parseFloat(price.dataset.price) * input.value;
 }
 
