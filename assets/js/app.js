c91a187aedffad96b14c48bf6f1d8409af2821f2
jueab1 <hoque273@gmail.com>
2024-05-06 22:39:33 +0600

    vaya please fixed the problem

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
+ 
+var header_cart_products = [
+  {
+    image_no: 1,
+     
+    
+  }
+]
+
+var Product_catagory_list = [
+  {
+    
+  }
+]
+
+
 function decrementCart(price_target) {
   event.preventDefault()
   let input = event.currentTarget.nextElementSibling;
